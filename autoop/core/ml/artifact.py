2edebf483531dfb53fdcbc25e39ddea976bbb3d9
JacomijnS <jasmijn.scholtens2002@gmail.com>
2024-10-23 14:02:59 +0200

    added to artifact

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -2,15 +2,15 @@ from pydantic import BaseModel, Field
 import base64
 
 class Artifact(BaseModel):
-
-    name: str
-    version: str
-    asset_path: str
-    tags: list
-    metadata: str
-    type: str
-    data: list
-    _id: str
+    def __init__(self, name: str, data: bytes):
+        name: str
+        version: str
+        asset_path: str
+        tags: list
+        metadata: str
+        type: str
+        data: list
+        _id: str
 
     @property
     def id(self, asset_path: str, version: str) -> str:
@@ -18,4 +18,28 @@ class Artifact(BaseModel):
         _id = base64.b64encode(_id)
         _id = _id  + ':' + version
         return _id
-    
+
+    @property
+    def save(self, data: bytes) -> bytes:
+        return data
+
+    @property
+    def read(self):
+        return self.data
+
+    @property
+    def get(self, type: str):
+        return self.data
+
+# with json you can convert the object to a string and then bytes and save it to a file
+# data should be in bytes
+# saved artifact should be in data
+
+# model.dump
+# baseclass for all the logical objects in the pipeline
+# define an abstract method to dump the object to a file
+# we want to have files that refers to this specific type of attr
+# be able to save dataset, model, features
+# find what the attr mean by checking the requirements and specific code
+# very generic abstract class
+# manage all types of artifacts
