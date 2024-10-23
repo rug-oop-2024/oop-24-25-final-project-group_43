95d2388cde34c9b81fae8dc12988ef0242ae42c8
bladz0r <d.drlic@student.rug.nl>
2024-10-23 08:16:27 +0200

    added artifact class

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -2,6 +2,20 @@ from pydantic import BaseModel, Field
 import base64
 
 class Artifact(BaseModel):
-    pass
 
-#model.dump
+    name: str
+    version: str
+    asset_path: str
+    tags: list
+    metadata: str
+    type: str
+    data: list
+    _id: str
+
+    @property
+    def id(self, asset_path: str, version: str) -> str:
+        _id = asset_path.encode('ascii')
+        _id = base64.b64encode(_id)
+        _id = _id  + ':' + version
+        return _id
+    
