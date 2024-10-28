7f3ff3a2b8c0e7f7af1effe4e66af945dbda3ee7
David <d.drlic@student.rug.nl>
2024-10-28 07:33:23 +0100

    imported models

--- autoop/core/ml/model/__init__.py
+++ autoop/core/ml/model/__init__.py
@@ -1,11 +1,22 @@
 
 from autoop.core.ml.model.model import Model
-from autoop.core.ml.model.regression import MultipleLinearRegression
+from autoop.core.ml.model.regression.multiple_linear_regression import MultipleLinearRegression
+from autoop.core.ml.model.regression.polynominal import PolynomialRegression
+from autoop.core.ml.model.regression.lasso import LassoWrapper
+from autoop.core.ml.model.classification.knn import KNN
+from autoop.core.ml.model.classification.logistic_regression import LogisticRegressionWrapper
+from autoop.core.ml.model.classification.random_forest import RandomForest
 
 REGRESSION_MODELS = [
+    "Lasso",
+    "Multiple Linear Regression",
+    "Polynomial"
 ] # add your models as str here
 
 CLASSIFICATION_MODELS = [
+    "KNN",
+    "Logistic Regression",
+    "Random forest"
 ] # add your models as str here
 
 def get_model(model_name: str) -> Model:
