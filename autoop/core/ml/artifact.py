d304a6e7503e25f91412eaf8216fa30d4603472c
David <d.drlic@student.rug.nl>
2024-10-28 07:35:25 +0100

    artifact is still a work in progress

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -1,16 +1,20 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 import base64
+from typing import Literal, Optional
+import pandas as pd
 
 class Artifact(BaseModel):
-    def __init__(self, name: str, data: bytes):
-        name = name
-        version: str
-        asset_path: str
-        tags: list
-        metadata: str
-        type: str
-        data = data
-        _id: str
+
+    #def __init__(self, type: str, name: str, asset_path: str, data: bytes, version: str):
+    type: Literal['dataset', 'OneHotEncoder', 'StandardScaler']
+    name: str
+    asset_path: str
+    version: str
+    tags: Optional[list[str]] = None
+    data: Optional[bytes]
+    metadata: Optional[list[str]] = None
+    _id: str
+
 
     @property
     def id(self, asset_path: str, version: str) -> str:
@@ -23,16 +27,21 @@ class Artifact(BaseModel):
     # They are not done yet, just concepts of what should be according to pipeline.py
     @property
     def save(self, data: bytes) -> bytes:
+        if not isinstance(data, bytes):
+            raise TypeError(f"Data should be of type bytes, found {data.__class__.__name__}")
         return data
 
     @property
     def read(self):
-        return self.data
+        return self
+        
 
     @property
-    def get(self, type: str):
-        return self.data
+    def get(self, param: str):
+        if param == "type":
+            return self.type
     
+
     def dump(self, data: bytes):
         return data
 
