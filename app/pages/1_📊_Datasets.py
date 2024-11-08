d24700b253cbba0c7daab12af7cc327e829fb67e
David <d.drlic@student.rug.nl>
2024-11-08 07:17:17 +0100

    added 'datasets/' to asset_path

--- app/pages/1_📊_Datasets.py
+++ app/pages/1_📊_Datasets.py
@@ -6,6 +6,7 @@ import os
 # Ensure the autoop module is in the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
+
 from app.core.system import AutoMLSystem
 from autoop.core.ml.dataset import Dataset
 
@@ -13,14 +14,18 @@ automl = AutoMLSystem.get_instance()
 
 datasets = automl.registry.list(type="dataset")
 
+
+
 st.write("# 📊 Datasets")
 # your code here
-st.write("Upload your dataset to get started.")
+
+st.subheader("Upload your dataset to get started.")
 file = st.file_uploader("Upload a CSV file", type=["csv"])
 if file is not None:
     df = pd.read_csv(file)
     st.write(df)
-    dataset = Dataset.from_dataframe(df, name=file.name[:-4], asset_path=file.name)
+    dataset = Dataset.from_dataframe(df, name=file.name[:-4], asset_path=f'datasets/{file.name}')
     automl.registry.register(dataset)
+
     
     st.success("Dataset uploaded successfully.")
