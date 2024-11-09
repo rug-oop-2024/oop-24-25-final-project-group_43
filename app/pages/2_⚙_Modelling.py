45050b4cd0320d1352c6e210ddbbb6fdbfb76d00
David <d.drlic@student.rug.nl>
2024-11-09 10:54:07 +0100

    created functions

--- app/pages/2_⚙_Modelling.py
+++ app/pages/2_⚙_Modelling.py
@@ -27,13 +27,8 @@ automl = AutoMLSystem.get_instance()
 datasets = automl.registry.list(type="dataset")
 
 
-# Select a dataset from uploaded datasets
-st.subheader("Dataset Selection")
-#dataset_names = [dataset.name for dataset in datasets]
-selected_dataset_name = st.selectbox("Select a dataset",
-                                      [dataset.name for dataset in datasets])
-selected_dataset = next(dataset for dataset in datasets 
-                        if dataset.name == selected_dataset_name)
+
+
 
 """
 if st.button('Refresh Datasets'):
@@ -44,8 +39,16 @@ if st.button('Refresh Datasets'):
 ############################################################################################################
 # Styled code with the use of functions
 ############################################################################################################
-"""
-def load_dataset():
+
+def select_dataset(datasets):
+    selected_dataset_name = st.selectbox("Select a dataset",
+                                      [dataset.name for dataset in datasets])
+    selected_dataset = next(dataset for dataset in datasets 
+                            if dataset.name == selected_dataset_name)
+    return selected_dataset
+
+
+def load_dataset(selected_dataset):
     st.write(f"Selected dataset: {selected_dataset.name}")
 
     # Load the dataset (couldn't figure out how to load the dataset from the registry, 
@@ -55,19 +58,16 @@ def load_dataset():
     st.write(df)
     datasett = Dataset.from_dataframe(df, name=selected_dataset.name, 
                                       asset_path=selected_dataset.asset_path)
+
+    return datasett
+
+
+def load_features(datasett):
     features = detect_feature_types(datasett)
+    return features
 
-    feature_names = [feature.name for feature in features]
-    
-    st.subheader("Feature Selection")
 
-    # Generate selection menus for input features and target feature
-    input_features = st.multiselect("Select input features", feature_names)
-    target_feature = st.selectbox("Select target feature", feature_names)
-    if input_features and target_feature:
-        determine_task_type()
-
-def determine_task_type():
+def determine_task_type(input_features, target_feature, features):
     if input_features and target_feature:
     # Determine task type based on target feature
         target_feature_type = next((feature.type for feature in features if 
@@ -79,133 +79,110 @@ def determine_task_type():
             task_type = "classification"
         
         st.write(f"Detected task type: {task_type}")
-"""
-############################################################################################################
-# original code
-############################################################################################################
-
-
-if selected_dataset:
-    st.write(f"Selected dataset: {selected_dataset.name}")
-
-    # Load the dataset (couldn't figure out how to load the dataset from the registry, 
-    # since it is a list of artifacts)
-    file_path = os.path.join('assets/objects/', selected_dataset.asset_path)
-    df = pd.read_csv(file_path)
-    st.write(df)
-    datasett = Dataset.from_dataframe(df, name=selected_dataset.name, 
-                                      asset_path=selected_dataset.asset_path)
-    features = detect_feature_types(datasett)
-
-    feature_names = [feature.name for feature in features]
+        return task_type
     
-    st.subheader("Feature Selection")
+def select_model(task_type):
+    st.subheader("Model Selection")
+    if task_type == "classification":
+        model_type = st.selectbox("Select a "\
+            "classification model", ["KNN", "Logistic Regression", "Random Forest"])
+    elif task_type == "regression":
+        model_type = st.selectbox("Select a "\
+            "regression model", ["Lasso", "Multiple Linear Regression",
+                                    "Polynomial Regression"])
+        
+    return model_type
 
-    # Generate selection menus for input features and target feature
-    input_features = st.multiselect("Select input features", feature_names)
-    target_feature = st.selectbox("Select target feature", feature_names)
+def split_data():
+    st.subheader("Splitting Data")
+    split_ratio = st.slider("Select the training set split ratio",
+                            0.1, 0.9, 0.5, 0.05)
+    return split_ratio
+
+def select_metrics(task_type):
+    st.subheader("Metrics Selection")
+    if task_type == "classification":
+        metrics = st.multiselect("Select metrics for classification",
+                                ["Accuracy", "Recall", "Precision"])
+    elif task_type == "regression":
+        metrics = st.multiselect("Select metrics for regression",
+                                ["Mean Squared Error",
+                                    "Root Mean Squared Error",
+                                    "Mean Absolute Error"])
+    return metrics
+
+def display_summary(selected_dataset_name, input_features, target_feature, model_type, split_ratio, metrics):
+    if st.button("Show Summary"):
+        st.write(f"Selected dataset: {selected_dataset_name}")
+        st.write(f"Selected input features: {input_features}")
+        st.write(f"Selected target feature: {target_feature}")
+        st.write(f"Selected model: {model_type}")
+        st.write(f"Training set split ratio: {split_ratio}")
+        st.write(f"Selected metrics: {metrics}")
+
+def train_model(datasett, features,  input_features, target_feature, model_type, split_ratio, metrics):
+    if st.button("Train Model"):
+        st.write("Training model...")
+        input_features = [next(feature for feature in features 
+                                if feature.name == feature_name) 
+                                for feature_name in input_features]
+        target_feature = next(feature for feature in features 
+                                if feature.name == target_feature)
+        metrics = [get_metric(metric) for metric in metrics]
+        pipeline = Pipeline(metrics=metrics, dataset=datasett,
+                            model=get_model(model_type),
+                                input_features=input_features,
+                                target_feature=target_feature, 
+                                split=split_ratio)
+        result = pipeline.execute()
+        st.write("Model trained successfully.")
+        return result
+
+
+
+def print_result(result):
+
+    metrics = result["metrics"]
+    metrics_train = result["metrics_train"]
+
+    metric_values = [metric[1].item() for metric in metrics]
+    metric_names = [str(metric[0]).split('.')[4].split(' ')[0] for metric in metrics]
+
+    metric_train_values = [metric[1].item() for metric in metrics_train]
+    metric_train_names = [str(metric[0]).split('.')[4].split(' ')[0] for metric in metrics_train]
     
-    if input_features and target_feature:
-    # Determine task type based on target feature
-        target_feature_type = next((feature.type for feature in features if 
-                                    feature.name == target_feature), None)
+
+    st.subheader("Metrics:")
+    for names, values in zip(metric_names, metric_values):
+        st.write(f"{names}: {values}")
+
+    st.subheader("Training Metrics:")
+    for names, values in zip(metric_train_names, metric_train_values):
+        st.write(f"{names}: {values}")
+
+
+############################################################################################################
+# "main" 
+############################################################################################################
+
+
+st.subheader("Dataset Selection")
+selected_dataset = select_dataset(datasets)
+datasett = load_dataset(selected_dataset)
+features = load_features(datasett)
+
+feature_names = [feature.name for feature in features]
+st.subheader("Feature Selection")
+input_features = st.multiselect("Select input features", feature_names)
+target_feature = st.selectbox("Select target feature", feature_names)
+
+if input_features and target_feature:
+        task_type = determine_task_type(input_features, target_feature, features)
+        model_type = select_model(task_type)
+        split_ratio = split_data() 
+        metrics = select_metrics(task_type)
+        display_summary(selected_dataset.name, input_features, target_feature, model_type, split_ratio, metrics)
+        result = train_model(datasett, features, input_features, target_feature, model_type, split_ratio, metrics)
+        if result:
+            print_result(result)
         
-        if target_feature_type == 'numerical':
-            task_type = "regression"
-        else:
-            task_type = "classification"
-        
-        st.write(f"Detected task type: {task_type}")
-
-
-        # Select model type based on task type
-        st.subheader("Model Selection")
-        if task_type == "classification":
-            model_type = st.selectbox("Select a "\
-                "classification model", ["KNN", "Logistic Regression", "Random Forest"])
-        elif task_type == "regression":
-            model_type = st.selectbox("Select a "\
-                "regression model", ["Lasso", "Multiple Linear Regression",
-                                      "Polynomial Regression"])  
-
-
-        # Splitting Data
-        st.subheader("Splitting Data")
-        split_ratio = st.slider("Select the training set split ratio",
-                                 0.1, 0.9, 0.5, 0.05)
-
-
-        # Select metrics based on task type
-        st.subheader("Metrics Selection")
-        if task_type == "classification":
-            metrics = st.multiselect("Select metrics for classification",
-                                      ["Accuracy", "Recall", "Precision"])
-        elif task_type == "regression":
-            metrics = st.multiselect("Select metrics for regression",
-                                      ["Mean Squared Error",
-                                        "Root Mean Squared Error",
-                                          "Mean Absolute Error"])
-
-
-        # Display summary of selected options
-        if st.button("Show Summary"):
-            st.write(f"Selected dataset: {selected_dataset_name}")
-            st.write(f"Selected input features: {input_features}")
-            st.write(f"Selected target feature: {target_feature}")
-            st.write(f"Selected model: {model_type}")
-            st.write(f"Training set split ratio: {split_ratio}")
-            st.write(f"Selected metrics: {metrics}")
-
-
-        # Train the model
-        if st.button("Train Model"):
-            st.write("Training model...")
-            input_features = [next(feature for feature in features 
-                                   if feature.name == feature_name) 
-                                   for feature_name in input_features]
-            target_feature = next(feature for feature in features 
-                                  if feature.name == target_feature)
-            metrics = [get_metric(metric) for metric in metrics]
-            pipeline = Pipeline(metrics=metrics, dataset=datasett,
-                                 model=get_model(model_type),
-                                    input_features=input_features,
-                                    target_feature=target_feature, 
-                                    split=split_ratio)
-            result = pipeline.execute()
-            st.write(result)
-
-
-            # Errors: 
-            # KNN:     
-                #    return np.bincount(k_nearest_labels).argmax()
-                # TypeError: Cannot cast array data from dtype('float64') to dtype('int64') according to the rule 'safe'
-
-            # Logistic Regression:
-                # y = column_or_1d(y, warn=True)
-                # raise ValueError(
-                # ValueError: y should be a 1d array, got an array of shape (75, 3) instead.
-
-
-
-            # Train the model
-            # Evaluate the model
-            # Display evaluation results
-            st.write("Model trained successfully.")
-
-
-                # def __init__(self, 
-                #  metrics: List[Metric],
-                #  dataset: Dataset, 
-                #  model: Model,
-                #  input_features: List[Feature],
-                #  target_feature: Feature,
-                #  split=0.8,
-                #  ):
-
-else:
-    st.write("No dataset selected.")
-
-
-
-
-
